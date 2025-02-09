b3d2c5ca4d9e3c0bcbd3061cc3a88f394e1eec69
LooJinnYang <ljinnyang@gmail.com>
2025-02-09 03:31:39 +0000

    Feb 9, 2025, 3:31 AM

--- CT06_04/lesson4.py
+++ CT06_04/lesson4.py
@@ -1,8 +1,9 @@
-red_plate = 1
-blue_plate = 2
-green_plate = 3
-num_of_R = 3
-num_of_B = 5
-num_of_G = 4
-total_price = (num_of_R * red_plate) + (num_of_B * blue_plate) + (num_of_G * green_plate)
-print (total_price)
+# red_plate = 1
+# blue_plate = 2
+# green_plate = 3
+# num_of_R = 3
+# num_of_B = 5
+# num_of_G = 4
+# total_price = (num_of_R * red_plate) + (num_of_B * blue_plate) + (num_of_G * green_plate)
+# print (total_price)
+
