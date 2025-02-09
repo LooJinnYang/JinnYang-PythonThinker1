ad82d561f1a5f2116784ef9b02957426ecf41165
LooJinnYang <ljinnyang@gmail.com>
2025-02-09 03:05:36 +0000

    Feb 9, 2025, 3:05 AM

--- CT06_03/lesson3.py
+++ CT06_03/lesson3.py
@@ -1,1 +1,66 @@
-print("Hello from lesson 3")
+#create variable Adam
+#set variable Adam to 60
+
+#create variable Brandon
+#set variable Brandon to 58
+
+#create variable Charlie
+#set variable Charlie to 98
+
+#create variable Daniel
+#set variable Daniel to 79
+
+#create variable AverageScore
+#set variable AverageScore to 0
+#add total of Adam, Brandon, Charlie and Daniel
+#divide thet total by the number of students(4) and assign to AverageScore
+#print variable AverageScore
+
+
+# age = 13
+# print (age)
+
+
+# "x" = 10
+# print ("x")
+
+
+# variable1 = 3
+# variable2 = 5
+# addition = variable1 + variable2
+# multiplication = variable1 * variable2
+# division = variable1 / variable2
+# differences = variable1 - variable2
+# print (addition)
+# print (multiplication)
+# print (division)
+# print (differences)
+
+
+# a = 64812658687657302067265620672460578203985786737584657365908150866557778876254654256356766887669785978998797658499976498764897648987648957689467487649674986749815628497638476942715647628476837482674817682767348287471623212321
+# b = 5632756872901785108416074165487301780410193174871893275183460901804328683632094968390602828603883041986006846685441383408041408601831570
+# sum = a + b
+# print (sum)
+
+
+# variable1 = 50
+# variable2 = 10
+# differences = variable1 - variable2
+# multiplier = 3
+# answer = differences * multiplier
+# print (answer)
+
+
+# variable1 = 100
+# variable2 = 4
+# answer = variable1 / variable2
+# print (answer)
+
+
+variable1 = 3
+variable2 = 4
+variable3 = 5
+variable4 = 10
+variable5 = 2
+answer = (variable1 + variable2) * variable3 - (variable4 / variable5)
+print (answer)
