cc0f5b1adc5d7b5d184f8ddd75a4e35af5dfcbe5
LooJinnYang <ljinnyang@gmail.com>
2025-02-09 04:28:31 +0000

    Feb 9, 2025, 4:28 AM

--- CT06_04/lesson4.py
+++ CT06_04/lesson4.py
@@ -36,7 +36,7 @@ num = str(num)
 print ("Double your number is " + num + ".")
 
 born = input ("What year were you born in?")
-born = int(age) +1
+born = int(born) +1
 age = str(age)
 print ("Next year, you will be " + age + " years old.")
 
