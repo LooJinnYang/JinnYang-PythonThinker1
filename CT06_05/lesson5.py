0a1e3e4b2d23ffd2813cc6546b9fbaad69f8fc74
LooJinnYang <ljinnyang@gmail.com>
2025-02-16 03:28:11 +0000

    Feb 16, 2025, 3:28 AM

--- CT06_05/lesson5.py
+++ CT06_05/lesson5.py
@@ -3,4 +3,4 @@
 name = input("whats your name")
 age = input("whats your age")
 message = input("whats your special message")
-print("happy " + age + "th birthday")
+print("happy " + age + "th birthday" + name + "")
