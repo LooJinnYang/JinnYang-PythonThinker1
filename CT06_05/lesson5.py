e80fb6acd6d9032d6e534ea4878e2e09efaf8d99
LooJinnYang <ljinnyang@gmail.com>
2025-02-16 03:44:06 +0000

    Feb 16, 2025, 3:44 AM

--- CT06_05/lesson5.py
+++ CT06_05/lesson5.py
@@ -5,8 +5,8 @@
 # message = input("whats your special message")
 # print("happy " + age + "th birthday " + name + " ! " + message)
 
-for num in range (10000000):
-    print("I will keep on slinging mud at my friends!")
+# for num in range (10000000):
+#     print("I will keep on slinging mud at my friends!")
 
 # input("whats your name")
 # print("")
