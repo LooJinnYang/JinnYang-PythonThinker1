1187684c1657c961c8e04aac04da96ba94cf1f9e
LooJinnYang <ljinnyang@gmail.com>
2025-02-16 04:11:12 +0000

    Feb 16, 2025, 4:11 AM

--- CT06_05/lesson5.py
+++ CT06_05/lesson5.py
@@ -26,8 +26,9 @@
 # for number in range (1,6):
 #      print (number)
 
-for number in range (51,101):
-     print (number)
+# for number in range (51,101):
+#      print (number)
 
 #  for number in range (18,30):
 #      print (number)
+
