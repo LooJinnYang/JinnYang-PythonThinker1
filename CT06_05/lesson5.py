90275509cdd04ba814eb9314ce93de0ff13e790d
LooJinnYang <ljinnyang@gmail.com>
2025-02-16 03:51:06 +0000

    Feb 16, 2025, 3:51 AM

--- CT06_05/lesson5.py
+++ CT06_05/lesson5.py
@@ -8,8 +8,8 @@
 # for num in range (10000000):
 #     print("I will keep on slinging mud at my friends!")
 
-name = input ("whats your name")
-for letter in name:
-    print ("give me a " + letter)
-print (name + " is the best")
+# name = input ("whats your name")
+# for letter in name:
+#     print ("give me a " + letter)
+# print (name + " is the best")
 
