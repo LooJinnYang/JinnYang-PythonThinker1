0a7712da8ead22debdf92ae8472a9f12c860284c
LooJinnYang <ljinnyang@gmail.com>
2025-02-23 03:14:04 +0000

    Feb 23, 2025, 3:14 AM

--- CT06_05/lesson5.py
+++ CT06_05/lesson5.py
@@ -65,5 +65,5 @@
 
 start = input("What is your starting number?")
 stop = input("What is your ending number?")
-for num in range (start,stop,):
-    print
+for time in range (start,stop,):
+    print 
