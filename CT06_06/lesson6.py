ae5b7dad17372b3cd0e85d446e457872fac1777c
LooJinnYang <ljinnyang@gmail.com>
2025-02-23 04:01:55 +0000

    Feb 23, 2025, 4:01 AM

--- CT06_06/lesson6.py
+++ CT06_06/lesson6.py
@@ -35,7 +35,7 @@
 num = input ("Choose a random number")
 num = int(num)
 for i in range(num,1,-1):
-    print (num * num * num)
+    print (num * num * num * num * num * num * num * num * num * num * num * num * num * num * num * num * num * num * num * num * num * num * num * num * num * num * num * num * num * num * num)
 
 # **Task 3a**:
 # age = int(25)
