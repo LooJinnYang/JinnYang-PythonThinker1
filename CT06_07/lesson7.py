061a13ee4cef86bd0d4a1c152362ba706353b13d
LooJinnYang <ljinnyang@gmail.com>
2025-03-02 04:25:17 +0000

    Mar 2, 2025, 4:25 AM

--- CT06_07/lesson7.py
+++ CT06_07/lesson7.py
@@ -49,5 +49,5 @@
 #     print(str(timesnum ) + " x " + str(n) + " = " + str(timesnum * n))
 
 num = int(input("Choose a random number: "))
-for n in range(1,num + 1,):
+for n in range(1,num + 1, num in range):
     print(n * num)
