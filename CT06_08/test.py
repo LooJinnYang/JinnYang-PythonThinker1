e477b133159753d4f8fb6fbabb65ca3a638bfca7
LooJinnYang <ljinnyang@gmail.com>
2025-03-09 03:34:15 +0000

    Mar 9, 2025, 3:34 AM

--- CT06_08/test.py
+++ CT06_08/test.py
@@ -3,6 +3,7 @@
 # #print out the name with additional string
 # print("Nice to meet you, " + name)
 
-#asking the user for inputs
+#asking the user for inputs for the range function
 start=int(input("What is your selected start number? "))
-
+start=int(input("What is your selected start number? "))
+start=int(input("What is your selected start number? "))
