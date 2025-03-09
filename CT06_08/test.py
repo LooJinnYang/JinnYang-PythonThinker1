cb2e8e7678c58b0ab6b0dadc78b896ae074615ed
LooJinnYang <ljinnyang@gmail.com>
2025-03-09 03:33:18 +0000

    Mar 9, 2025, 3:33 AM

--- CT06_08/test.py
+++ CT06_08/test.py
@@ -3,4 +3,4 @@
 # #print out the name with additional string
 # print("Nice to meet you, " + name)
 
-start=int(input("What is your selected start number"))
+start=int(input("What is your selected start number? "))
