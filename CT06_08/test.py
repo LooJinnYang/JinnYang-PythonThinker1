81093248f23a3b23278d27eb4dec805906f48b3a
LooJinnYang <ljinnyang@gmail.com>
2025-03-09 03:40:19 +0000

    Mar 9, 2025, 3:40 AM

--- CT06_08/test.py
+++ CT06_08/test.py
@@ -1,7 +1,3 @@
-#ask for name and assign to variable
-# name=input("What is your name? ")
-#print out the name with additional string
-# print("Nice to meet you, " + name)
 
 # #asking the user for inputs for the range function
 # start=int(input("What is your selected start number? "))
