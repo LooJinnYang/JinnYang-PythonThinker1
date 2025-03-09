cd99acb267f0c2410835983319588b16817c4363
LooJinnYang <ljinnyang@gmail.com>
2025-03-09 03:30:41 +0000

    Mar 9, 2025, 3:30 AM

--- CT06_08/test.py
+++ CT06_08/test.py
@@ -1,4 +1,4 @@
 #ask for name and assign to variable
 name=input("What is your name? ")
 #print out the name with additional string
-print("Nice to meet you " + name)
+print("Nice to meet you, " + name)
