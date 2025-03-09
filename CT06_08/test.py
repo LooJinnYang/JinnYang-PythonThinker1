a2599f6d32c33e2fed92c91384735f4ae0c559a7
LooJinnYang <ljinnyang@gmail.com>
2025-03-09 03:40:37 +0000

    Mar 9, 2025, 3:40 AM

--- CT06_08/test.py
+++ CT06_08/test.py
@@ -1,7 +1,7 @@
-# #asking the user for inputs for the range function
-# start=int(input("What is your selected start number? "))
-# stop=int(input("What is your selected stop number? "))
-# increment=int(input("What is your selected increment number? "))
+#asking the user for inputs for the range function
+start=int(input("What is your selected start number? "))
+stop=int(input("What is your selected stop number? "))
+increment=int(input("What is your selected increment number? "))
 
 # #giving the output using the user input
 # for i in range(start, stop, increment):
