5d38b13f87baaed058aa6f1587e74d634af6900d
LooJinnYang <ljinnyang@gmail.com>
2025-03-09 03:39:14 +0000

    Mar 9, 2025, 3:39 AM

--- CT06_08/test.py
+++ CT06_08/test.py
@@ -1,6 +1,6 @@
 # #ask for name and assign to variable
 # name=input("What is your name? ")
-# #print out the name with additional string
+#print out the name with additional string
 # print("Nice to meet you, " + name)
 
 #asking the user for inputs for the range function
@@ -8,6 +8,6 @@ start=int(input("What is your selected start number? "))
 stop=int(input("What is your selected stop number? "))
 increment=int(input("What is your selected increment number? "))
 
-#giving the output using the 
+#giving the output using the user input
 for i in range(start, stop, increment):
     print(i)
