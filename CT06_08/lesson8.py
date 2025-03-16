451af38e5aeed92ef388ec934dee67b39c310382
LooJinnYang <ljinnyang@gmail.com>
2025-03-16 03:43:47 +0000

    Mar 16, 2025, 3:43 AM

--- CT06_08/lesson8.py
+++ CT06_08/lesson8.py
@@ -5,3 +5,10 @@ for i in range(10,0,-1):
     print(i)
     time.sleep(1)
 print("Happy New Year")
+
+import time
+
+for i in range(10,0,-1):
+    print(i)
+    time.sleep(1)
+print("Happy New Year")
