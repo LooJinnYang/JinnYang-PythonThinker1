67f22250855ef508395ff2e71cb77188743203aa
LooJinnYang <ljinnyang@gmail.com>
2025-03-16 03:45:15 +0000

    Mar 16, 2025, 3:45 AM

--- CT06_08/lesson8.py
+++ CT06_08/lesson8.py
@@ -7,7 +7,7 @@
 # print("Happy New Year")
 
 import time
-num = int(input("How many seconds? "))
+num = int(input("How many seconds to countdown from? "))
 for i in range(num,0,-1):
     print(i)
     time.sleep(1)
