d9cccc3359628cea0e326a51fc547ec8a1744324
LooJinnYang <ljinnyang@gmail.com>
2025-03-16 04:29:42 +0000

    Mar 16, 2025, 4:29 AM

--- CT06_08/lesson8.py
+++ CT06_08/lesson8.py
@@ -52,4 +52,5 @@ num = int(input("How many questions ?"))
 for i in range(num):
     num1 = random.randint(1,10)
     num2 = random.randint(1,10)
-    print("What is " + str(num1) + "x" + str(num2) + )
+    print("What is " + str(num1) + "x" + str(num2) + " ?")
+    
