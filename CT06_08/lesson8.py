1bab5a914ab862b01926a89e99244b3e892c65a8
LooJinnYang <ljinnyang@gmail.com>
2025-03-16 04:07:05 +0000

    Mar 16, 2025, 4:07 AM

--- CT06_08/lesson8.py
+++ CT06_08/lesson8.py
@@ -35,7 +35,6 @@ variable1 = True
 variable2 = False
 print (variable1 == variable2)
 
-
-# import random
-# for i in range(20):
-#     print(random.randint(0,9999))
+import random
+for i in range(20):
+    print(random.randint(0,9999))
