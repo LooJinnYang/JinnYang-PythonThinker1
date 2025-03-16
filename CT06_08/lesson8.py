0d0bc901593f59c74dd630f616978f69118e3d9f
LooJinnYang <ljinnyang@gmail.com>
2025-03-16 04:23:20 +0000

    Mar 16, 2025, 4:23 AM

--- CT06_08/lesson8.py
+++ CT06_08/lesson8.py
@@ -47,3 +47,7 @@
 # print(guess == num1)
 # print("The actual number is " + str(num1))
 
+import random
+guess = int(input("guess a number from 1 to 10: "))
+num1 = random.randint(1,10)
+print(guess == num1)
