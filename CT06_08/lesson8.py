b9b7f8f2c307b750cf2f0653fbd6543c42d336e7
LooJinnYang <ljinnyang@gmail.com>
2025-03-16 03:44:41 +0000

    Mar 16, 2025, 3:44 AM

--- CT06_08/lesson8.py
+++ CT06_08/lesson8.py
@@ -1,10 +1,10 @@
 # print("Hello from lesson 8")
 
-import time
-for i in range(10,0,-1):
-    print(i)
-    time.sleep(1)
-print("Happy New Year")
+# import time
+# for i in range(10,0,-1):
+#     print(i)
+#     time.sleep(1)
+# print("Happy New Year")
 
 import time
 num = int(input("How many seconds? "))
