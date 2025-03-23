8a4b497515b68ac733255cba4757ec881e268830
LooJinnYang <ljinnyang@gmail.com>
2025-03-23 04:16:56 +0000

    Mar 23, 2025, 4:16 AM

--- CT06_09/lesson9.py
+++ CT06_09/lesson9.py
@@ -39,14 +39,14 @@
 
 num_apples = int(input("How many apples do you want to buy? "))
 num_oranges = int(input("How many oranges do you want to buy? "))
-if num_apples > 10:
+if num_apples > 5:
     print ("You will get a 10% discount for buying that many apples!")
     price = num_apples * 0.90
     print("the price of the " + str(num_apples) + " apples = $" + str(price))
 else:
     price = num_apples * 1
     print("the price of the " + str(num_apples) + " apples = $" + str(price))
-if num_apples > 10:
+if num_apples > 5:
     print ("You will get a 10% discount for buying that many apples!")
     price = num_apples * 0.90
     print("the price of the " + str(num_apples) + " apples = $" + str(price))
