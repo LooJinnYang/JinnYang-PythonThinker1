90cfb31c537e2a2462bfb2e66172faf897f950db
LooJinnYang <ljinnyang@gmail.com>
2025-03-23 04:19:52 +0000

    Mar 23, 2025, 4:19 AM

--- CT06_09/lesson9.py
+++ CT06_09/lesson9.py
@@ -48,9 +48,9 @@ else:
     print("the price of the " + str(num_apples) + " apples = $" + str(price))
 if num_oranges > 5:
     print ("You will get a 10% discount for buying that many oranges!")
-    price = num_oranges * 0.81
-    print("the price of the " + str(num_oranges) + " oranges = $" + str(price))
+    price2 = num_oranges * 0.81
+    print("the price of the " + str(num_oranges) + " oranges = $" + str(price2))
 else:
-    price = num_oranges * 0.90
-    print("the price of the " + str(num_oranges) + " oranges = $" + str(price))
-
+    price2 = num_oranges * 0.90
+    print("the price of the " + str(num_oranges) + " oranges = $" + str(price2))
+total_price = int(price)
