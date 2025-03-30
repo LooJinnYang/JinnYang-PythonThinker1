ae2a15c15023cfaa0ac2320f97e61e9f684d0d78
LooJinnYang <ljinnyang@gmail.com>
2025-03-30 03:55:53 +0000

    Mar 30, 2025, 3:55 AM

--- CT06_10/lesson10.py
+++ CT06_10/lesson10.py
@@ -1,9 +1,10 @@
 # print("Hello from lesson 10")
 
-num = int(input("Choose a random number: "))
-if num >-1:
-    print(str(num) + " is positive")
-else:
-    print(str(num) + " is negative")
+# num = int(input("Choose a random number: "))
+# if num >-1:
+#     print(str(num) + " is positive")
+# else:
+#     print(str(num) + " is negative")
 
 import random
+
