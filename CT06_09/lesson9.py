0e988b399a965da09037f108c1b5fcc9e77aae23
LooJinnYang <ljinnyang@gmail.com>
2025-03-30 03:20:42 +0000

    Mar 30, 2025, 3:20 AM

--- CT06_09/lesson9.py
+++ CT06_09/lesson9.py
@@ -59,6 +59,6 @@ positive_days = 0
 for i in range(7):
     temp = int(input("What is today's temperature"))
     if temp>30:
-        positive_days=+positive_days
+        positive_days+=positive_days
     temp=0
 print(str(positive_days))
