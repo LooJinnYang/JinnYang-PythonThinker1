c8eb09d9ea7db0a58097d48fdeecc03618047e09
LooJinnYang <ljinnyang@gmail.com>
2025-03-30 03:59:00 +0000

    Mar 30, 2025, 3:59 AM

--- CT06_10/lesson10.py
+++ CT06_10/lesson10.py
@@ -7,7 +7,7 @@
 #     print(str(num) + " is negative")
 
 import random
-num = random.randint(10)
+num = random.randint(1,10)
 guess = int(input("Guess a random number from 1 to 10 : "))
 if guess == num:
     print("Congratulations!! You did it!")
