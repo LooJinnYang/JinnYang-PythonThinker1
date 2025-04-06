3b18aad411b9bb05bbc455ce1b03ce34f650d5c8
LooJinnYang <ljinnyang@gmail.com>
2025-04-06 03:28:38 +0000

    Apr 6, 2025, 3:28 AM

--- CT06_11/lesson11.py
+++ CT06_11/lesson11.py
@@ -18,5 +18,5 @@
 #     print("X")
 
 num = int(input("Choose a random number: "))
-if num % 3 == 0 and num % 3 == 0:
+if num % 3 == 0 and num % 7 == 0: # both your conditions are the same
     print("The number is divisible by 3 and 7")
