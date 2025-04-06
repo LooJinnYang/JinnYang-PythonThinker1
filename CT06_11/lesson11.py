d014cbfccc87f17114f41c6f6add3bf21a6c784a
LooJinnYang <ljinnyang@gmail.com>
2025-04-06 03:09:38 +0000

    Apr 6, 2025, 3:09 AM

--- CT06_11/lesson11.py
+++ CT06_11/lesson11.py
@@ -6,6 +6,6 @@ if px <= 5:
 elif px <= 5:
     print("Are you sure you need this?")
 elif px <= 5:
-    print("Where are ou getting this money from?!")
+    print("Where are you getting this money from?!")
 else:
         print("Don't even think about it!")
