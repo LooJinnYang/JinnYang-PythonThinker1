638908590be68e066b4d3a5e8ccd0f3c87cbe814
LooJinnYang <ljinnyang@gmail.com>
2025-04-06 03:30:25 +0000

    Apr 6, 2025, 3:30 AM

--- CT06_11/lesson11.py
+++ CT06_11/lesson11.py
@@ -20,3 +20,7 @@
 num = int(input("Choose a random number: "))
 if num % 3 == 0 and num % 7 == 0:
     print("The number is divisible by 3 and 7")
+
+num = int(input("Choose a random number: "))
+if num % 3 == 0 and num % 7 == 0:
+    print("The number is divisible by 3 and 7")
