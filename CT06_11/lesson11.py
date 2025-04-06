423eb4a8d51ec05e242ed02a7f243b3fbcc9851d
LooJinnYang <ljinnyang@gmail.com>
2025-04-06 03:31:46 +0000

    Apr 6, 2025, 3:31 AM

--- CT06_11/lesson11.py
+++ CT06_11/lesson11.py
@@ -23,5 +23,5 @@ if num % 3 == 0 and num % 7 == 0:
 
 name1 = input("Choose a random number: ")
 name1 = input("Choose a random number: ")
-if name1 == James and name2 == Leong: # need to be string "James"
+if name1 == James and name2 == str(Leong): # need to be string "James"
     print("The number is divisible by 3 and 7")
