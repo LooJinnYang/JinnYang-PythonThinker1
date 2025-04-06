843f762f669f3752246b832fbae7d779e42d51a3
LooJinnYang <ljinnyang@gmail.com>
2025-04-06 03:10:17 +0000

    Apr 6, 2025, 3:10 AM

--- CT06_11/lesson11.py
+++ CT06_11/lesson11.py
@@ -3,9 +3,9 @@
 px=int(input("What is the price? "))
 if px <= 5:
     print("Sounds good!")
-elif px <= 5:
+elif px <= 50:
     print("Are you sure you need this?")
-elif px <= 5:
+elif px <= 500:
     print("Where are you getting this money from?!")
 else:
         print("Don't even think about it!")
