aecacbd33a3e39e6894001200e427521c37cdc82
LooJinnYang <ljinnyang@gmail.com>
2025-04-06 03:37:02 +0000

    Apr 6, 2025, 3:37 AM

--- CT06_11/lesson11.py
+++ CT06_11/lesson11.py
@@ -28,7 +28,7 @@
 
 rider1 = int(input("What is player 1's age: "))
 rider2 = int(input("What is player 2's age: "))
-if rider1 >= 18 and rider2 >= 18:
+if rider1 >= 18 or rider2 >= 18:
     print("O")
 else:
     print("X")
