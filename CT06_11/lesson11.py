ad9e7d2a2ff2cd16e28c89b8a28bc2cddd6229ff
LooJinnYang <ljinnyang@gmail.com>
2025-04-06 03:33:14 +0000

    Apr 6, 2025, 3:33 AM

--- CT06_11/lesson11.py
+++ CT06_11/lesson11.py
@@ -17,9 +17,9 @@
 # else:
 #     print("X")
 
-num = int(input("Choose a random number: "))
-if num % 3 == 0 and num % 7 == 0:
-    print("The number is divisible by 3 and 7")
+# num = int(input("Choose a random number: "))
+# if num % 3 == 0 and num % 7 == 0:
+#     print("The number is divisible by 3 and 7")
 
 name1 = input("First name: ")
 name2 = input("Last name: ")
