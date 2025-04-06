90be3bd0ae94312ce0c56171c939ec971bab498d
LooJinnYang <ljinnyang@gmail.com>
2025-04-06 03:15:05 +0000

    Apr 6, 2025, 3:15 AM

--- CT06_11/lesson11.py
+++ CT06_11/lesson11.py
@@ -13,6 +13,6 @@
 rider1 = int(input("What is player 1's height: "))
 rider2 = int(input("What is player 2's height: "))
 if rider1 and rider2 > 120:
-    print("okay")
+    print("O")
 else:
-    print("")
+    print("X")
