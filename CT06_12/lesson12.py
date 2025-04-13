3fd075249f141166cb2f2e23ad51f65dd4e7db70
LooJinnYang <ljinnyang@gmail.com>
2025-04-13 02:59:02 +0000

    Apr 13, 2025, 2:59 AM

--- CT06_12/lesson12.py
+++ CT06_12/lesson12.py
@@ -2,4 +2,4 @@
 
 num = int(input("Choose a random number: "))
 if num % 3 == 0 and num % 5 == 0:
-    print ("The number is divisible by 3 and 5!)
+    print ("The number is divisible by 3 and 5!")
