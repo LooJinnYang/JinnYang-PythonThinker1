80293b14668f950472bbc6f5e7bb0274808ed99c
LooJinnYang <ljinnyang@gmail.com>
2025-04-13 04:26:32 +0000

    Apr 13, 2025, 4:26 AM

--- CT06_12/lesson12.py
+++ CT06_12/lesson12.py
@@ -73,5 +73,5 @@
 import random
 num1 = random.randint(1,10)
 num2 = random.randint(1,10)
-print("What is " + str(num1) + "+ " + str(num2) + "? ")
 sum = num1 + num2
+print("What is " + str(num1) + "+ " + str(num2) + "? ")
