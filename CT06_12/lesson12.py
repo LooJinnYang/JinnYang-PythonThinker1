f65366fc4ddbcbfe0e18611572eb3dee785bdf0a
LooJinnYang <ljinnyang@gmail.com>
2025-04-13 03:07:19 +0000

    Apr 13, 2025, 3:07 AM

--- CT06_12/lesson12.py
+++ CT06_12/lesson12.py
@@ -1,10 +1,10 @@
 # print("Hello from lesson 12")
 
-num = int(input("Choose a random number: "))
-if num % 3 == 0 and num % 5 == 0:
-    print ("The number is divisible by 3 and 5!")
-else:
-    print ("The number is not divisible by 3 and 5!")
+# num = int(input("Choose a random number: "))
+# if num % 3 == 0 and num % 5 == 0:
+#     print ("The number is divisible by 3 and 5!")
+# else:
+#     print ("The number is not divisible by 3 and 5!")
 
 vis = 0
 while vis < 50:
