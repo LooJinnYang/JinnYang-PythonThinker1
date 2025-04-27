56ce8bc00066b7df92010a1d18b3c1bc6179c6c7
LooJinnYang <ljinnyang@gmail.com>
2025-04-27 04:21:06 +0000

    Apr 27, 2025, 4:21 AM

--- CT06_14/lesson14.py
+++ CT06_14/lesson14.py
@@ -35,6 +35,19 @@
 # t.penup()
 # window.mainloop()
 
+# import turtle
+# window = turtle.Screen()
+# window.setup(width = 600, height = 400)
+# t = turtle.Turtle()
+# t.shape("turtle")
+# t.fillcolor("green")
+# t.pendown()
+# for i in range(6):
+#     t.forward(100)
+#     t.right(60)
+# t.penup()
+# window.mainloop()
+
 import turtle
 window = turtle.Screen()
 window.setup(width = 600, height = 400)
