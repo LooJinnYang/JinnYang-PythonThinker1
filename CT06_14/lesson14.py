2557659fff618db24d3ad019f2dca6ab8a306b49
LooJinnYang <ljinnyang@gmail.com>
2025-04-27 04:05:41 +0000

    Apr 27, 2025, 4:05 AM

--- CT06_14/lesson14.py
+++ CT06_14/lesson14.py
@@ -5,10 +5,10 @@
 # window.setup(width = 600, height = 400)
 # window.mainloop()
 
-# import turtle
-# window = turtle.Screen()
-# window.setup(width = 600, height = 400)
-# t = turtle.Turtle()
-# t.shape("square")
-# t.fillcolor("green")
-# window.mainloop()
+import turtle
+window = turtle.Screen()
+window.setup(width = 600, height = 400)
+t = turtle.Turtle()
+t.shape("square")
+t.fillcolor("green")
+window.mainloop()
