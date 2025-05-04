ddd2c5c30bda5003d637be50b40be516b869b736
LooJinnYang <ljinnyang@gmail.com>
2025-05-04 03:52:05 +0000

    May 4, 2025, 3:52 AM

--- CT06_15/test.py
+++ CT06_15/test.py
@@ -37,36 +37,37 @@
 
 ##########################################
 
-# Basic List Functions
-# Do the following operations to the list provided below
-# Write the code below each question.
+# # Basic List Functions
+# # Do the following operations to the list provided below
+# # Write the code below each question.
 
-planets = ["mercury","venus", "earth", "mars", "jupiter", "saturn", "uranus" ]
+# planets = ["mercury","venus", "earth", "mars", "jupiter", "saturn", "uranus" ]
 
-# 1. Write code below to print the 3rd item 
-#    in this list using index e.g. earth
+# # 1. Write code below to print the 3rd item 
+# #    in this list using index e.g. earth
 
-print(planets[2])
+# print(planets[2])
 
-# 2. Write code to append neptune to this list.
+# # 2. Write code to append neptune to this list.
 
-planets.append("neptune")
+# planets.append("neptune")
 
-# 3. Elon Musk has conquered Mars. 
-#    Rename Mars in the list to be "muskworld"
+# # 3. Elon Musk has conquered Mars. 
+# #    Rename Mars in the list to be "muskworld"
 
-planets[3] = "muskworld"
+# planets[3] = "muskworld"
 
-# 4. Remove uranus from this list.
+# # 4. Remove uranus from this list.
 
-# removing from new list
+# # removing from new list
 
-del(planets[6])
+# del(planets[6])
 
-# 5. Using a for loop, print all the planets 
-#    from this list one by one.
+# # 5. Using a for loop, print all the planets 
+# #    from this list one by one.
 
-print("Planets: ")
-for i in range (len(planets)):
-    print(str(i + 1) + "." + planets[i])
+# print("Planets: ")
+# for i in range (len(planets)):
+#     print(str(i + 1) + "." + planets[i])
 
+word = "hello"
