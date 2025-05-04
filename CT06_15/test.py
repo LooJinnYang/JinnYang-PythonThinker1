660f503e23bf7f6fb09ec34ef5c922b42a5d0018
LooJinnYang <ljinnyang@gmail.com>
2025-05-04 03:39:32 +0000

    May 4, 2025, 3:39 AM

--- CT06_15/test.py
+++ CT06_15/test.py
@@ -29,7 +29,9 @@ while i > 9 and i < 201:
 password = "superpass123"
 var = input("Insert the password here: ")
 if not password == var:
-    print("Try again")
+    print("Access Denied")
+else:
+    print
 
 
 
