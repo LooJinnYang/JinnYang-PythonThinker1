f2169e4d5b86555d3c5db671521d42d2f857d567
LooJinnYang <ljinnyang@gmail.com>
2025-05-11 04:30:08 +0000

    May 11, 2025, 4:30 AM

--- CT06_15/lesson15.py
+++ CT06_15/lesson15.py
@@ -55,4 +55,5 @@ def square(num):
 def square_of_sum(num1,num2):
     return square(num1) + square(num2)
 inp1 = int(input("First number: "))
+inp2 = int(input("Second number: "))
 print(square_of_sum(inp1,inp2))
