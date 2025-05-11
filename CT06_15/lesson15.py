a26e270aa52e5e59427435ce165ab1093593a53d
LooJinnYang <ljinnyang@gmail.com>
2025-05-11 04:13:50 +0000

    May 11, 2025, 4:13 AM

--- CT06_15/lesson15.py
+++ CT06_15/lesson15.py
@@ -11,27 +11,28 @@
 
 # print(counter)
 
-# Task 7: Even or Odd? III
-# Create a function that takes in a number and returns whether it is
-# even
+# # Task 7: Even or Odd? III
+# # Create a function that takes in a number and returns whether it is
+# # even
 
-# 1. Create a function named isEven()
-def is_Even(num):
+# # 1. Create a function named isEven()
+# def is_Even(num):
 
-# 2. If the number is even, the function should return True
-# 3. If the number is odd, the function should return False
-    return num % 2 == 0
+# # 2. If the number is even, the function should return True
+# # 3. If the number is odd, the function should return False
+#     return num % 2 == 0
 
-# 4. Using the 'isEven()' function, loop through a list of numbers and
-#    print them out in this format:
-#     "3 is an odd number"
-#     "9 is an odd number"
-#     "2 is an even number"
+# # 4. Using the 'isEven()' function, loop through a list of numbers and
+# #    print them out in this format:
+# #     "3 is an odd number"
+# #     "9 is an odd number"
+# #     "2 is an even number"
 
-nums = [3,5,8,326,4,]
+# nums = [3,5,8,326,4,]
 
-for num in (nums):
-    if is_Even(num):
-        print(str(num) + " is an even number.")
-    else:
-        print(str(num) + " is an odd number.")
+# for num in (nums):
+#     if is_Even(num):
+#         print(str(num) + " is an even number.")
+#     else:
+#         print(str(num) + " is an odd number.")
+
