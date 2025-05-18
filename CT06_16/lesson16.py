d4ace8155617bb07e127698e9a2ca2e4dd6caba7
LooJinnYang <ljinnyang@gmail.com>
2025-05-18 03:35:28 +0000

    May 18, 2025, 3:35 AM

--- CT06_16/lesson16.py
+++ CT06_16/lesson16.py
@@ -1,5 +1,26 @@
 # print("Hello from lesson 16")
 
+# import turtle
+
+# window = turtle.Screen()
+# window.setup(width = 600, height = 400)
+# t = turtle.Turtle()
+# t.shape("turtle")
+# t.fillcolor("green")
+
+# def drawShape(times, length):
+#     ang = 360 / times
+#     t.pendown()
+#     for i in range(times):
+#         t.forward(length)
+#         t.right(ang)
+#     t.penup()
+
+# times = int(input("How many sides does your polygon have: "))
+# length = int(input("How long does your turtle move: "))
+
+# drawShape(times, length)
+# window.mainloop()
 
 import turtle
 
