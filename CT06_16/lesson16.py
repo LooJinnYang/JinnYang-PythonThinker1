d4109b6f823c7142392b5ad01959ad340f4cff9c
LooJinnYang <ljinnyang@gmail.com>
2025-05-18 03:21:42 +0000

    May 18, 2025, 3:21 AM

--- CT06_16/lesson16.py
+++ CT06_16/lesson16.py
@@ -4,15 +4,14 @@ import turtle
 
 def drawShape(imes):
     ang = 360 / imes
-
+    t.pendown()
+    for i in range(imes):
+        t.forward(100)
+        t.right(ang)
+    t.penup()
 window = turtle.Screen()
 window.setup(width = 600, height = 400)
 t = turtle.Turtle()
 t.shape("turtle")
 t.fillcolor("green")
-t.pendown()
-for i in range(8):
-    t.forward(100)
-    t.right(45)
-t.penup()
 window.mainloop()
