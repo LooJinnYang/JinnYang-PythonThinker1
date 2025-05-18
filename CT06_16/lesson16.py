237ce9e12186113f071d1133ee47cff8adefb59c
LooJinnYang <ljinnyang@gmail.com>
2025-05-18 03:15:53 +0000

    May 18, 2025, 3:15 AM

--- CT06_16/lesson16.py
+++ CT06_16/lesson16.py
@@ -1,3 +1,16 @@
 # print("Hello from lesson 16")
 
-import turtle
+# import turtle
+# # import turtle
+# # window = turtle.Screen()
+# # window.setup(width = 600, height = 400)
+# # t = turtle.Turtle()
+# # t.shape("turtle")
+# # t.fillcolor("green")
+# # t.pendown()
+# # for i in range(5):
+# #     t.forward(100)
+# #     t.right(90)
+# # t.forward(40)
+# # t.penup()
+# # window.mainloop()
